d71d54188f028c06de5348a313c352bc9dfeac09
Kenneth Davis <kennethdavis391@gmail.com>
2019-11-18 15:00:39 -0500

    dependencies installed and models created

---
+++ Project-0/src/models/reimbursement.ts
@@ -0,0 +1,21 @@
+export class Reimbursement{
+    reimbursementId: number // primary key
+	author: number  // foreign key -> User, not null
+	amount: number  // not null
+    dateSubmitted: number // not null
+    dateResolved: number // not null
+    description: string // not null
+    resolver: number // foreign key -> User
+    status: number // foreign key -> ReimbursementStatus, not null
+    type: number // foreign key -> ReimbursementType
+}
+
+export class ReimbursementStatus{
+    statusId: number // primary key
+    status: string // not null, unique
+}
+
+export class ReimbursementType{
+    typeId: number // primary key
+    type: string // not null, unique
+}

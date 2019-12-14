604e82a62a06c9b8b64644330ab4d5cef86c1907
Kenneth Davis <kennethdavis391@gmail.com>
2019-12-13 22:46:07 -0500

    please let me get token from the headers now

--- Project-0/src/middleware/cors-middleware.ts
+++ Project-0/src/middleware/cors-middleware.ts
@@ -2,8 +2,9 @@ import {Request} from 'express'
 
 export function corsFilter(req:Request, res, next){
     res.header('Access-Control-Allow-Origin', `${req.headers.origin}`);
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept,');
     res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Expose-Headers', 'token')
     if(req.method === 'OPTIONS'){
         res.status(200).send();
     }else{

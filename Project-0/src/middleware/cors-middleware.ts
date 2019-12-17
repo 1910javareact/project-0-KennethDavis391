b1ca6910927abeb7896b48fdb38e9e937eff9cd9
Kenneth Davis <kennethdavis391@gmail.com>
2019-12-17 15:42:25 -0500

    allows token in headers

--- Project-0/src/middleware/cors-middleware.ts
+++ Project-0/src/middleware/cors-middleware.ts
@@ -2,7 +2,7 @@ import { Request } from 'express'
 
 export function corsFilter(req: Request, res, next){
     res.header('Access-Control-Allow-Origin', `${req.headers.origin}`);
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, token');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Expose-Headers', 'token')
     if (req.method === 'OPTIONS'){

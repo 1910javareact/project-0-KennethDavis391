da88cf551f83b4667d43c2119357cc89cd50faef
Kenneth Davis <kennethdavis391@gmail.com>
2019-12-22 10:51:29 -0500

    problem not fixed

--- Project-0/src/middleware/cors-middleware.ts
+++ Project-0/src/middleware/cors-middleware.ts
@@ -6,7 +6,6 @@ export function corsFilter(req: Request, res, next){
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Expose-Headers', 'token')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH')
-    res.header('Cache-Control','no-cache')
     if (req.method === 'OPTIONS'){
         res.status(200).send();
     } else {
